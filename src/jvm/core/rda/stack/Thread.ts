d329702d9c8e34fe4c1d54c617fc0142b60c5de5
itsu-dev <itsu020402@gmail.com>
2022-05-09 11:29:42 +0900

    Refactoring

--- src/jvm/core/rda/stack/Thread.ts
+++ src/jvm/core/rda/stack/Thread.ts
@@ -1,34 +1,33 @@
 import {Frame} from "./Frame.js";
-import {getArgumentsAndReturnType, throwErrorOrException} from "../jvm.js";
-import {ConstantPoolInfo, readUtf8FromConstantPool} from "./info/ConstantPoolInfo.js";
-import {MethodInfo} from "./info/MethodInfo.js";
-import {CodeAttribute} from "./info/AttributeInfo.js";
-import {ArrayVariable} from "./Variable.js";
-import {ClassFile} from "./ClassFile.js";
-import {System} from "../lib/java/lang/System.js";
-
-export type Opcode = {
-    id: number,
-    opcode: number,
-    operands: Array<number>
-}
+import {getArgumentsAndReturnType, throwErrorOrException} from "../../../jvm.js";
+import {ConstantPoolInfo, readUtf8FromConstantPool} from "../../../models/info/ConstantPoolInfo.js";
+import {MethodInfo} from "../../../models/info/MethodInfo.js";
+import {CodeAttribute} from "../../../models/info/AttributeInfo.js";
+import {ArrayVariable} from "../../../models/Variable.js";
+import {ClassFile} from "../../cfl/ClassFile.js";
+import {System} from "../../../lib/java/lang/System.js";
+import RuntimeDataArea from "../RuntimeDataArea.js";
 
 export default class Thread {
 
-    pc: number = 0;
-    stack: Array<Frame>
-    stackSize: number
+    runtimeDataArea: RuntimeDataArea;
+    stack: Array<Frame>;
+    id: number;
+    stackSize: number;
 
-    constructor(stackSize: number) {
+    constructor(runtimeDataArea: RuntimeDataArea, stackSize: number, id: number) {
         if (stackSize < 1) {
             System.err.println("StackSize must must be bigger than 1.");
             return;
         }
+        this.runtimeDataArea = runtimeDataArea;
         this.stackSize = stackSize;
         this.stack = [];
+        this.id = id;
     }
 
-    invokeMethod(methodName: string, constantPool: ConstantPoolInfo[], classFile: ClassFile, args: Array<any>) {
+    invokeMethod(methodName: string, classFile: ClassFile, args: Array<any>) {
+        const constantPool = classFile.constantPool;
         const method = classFile.methods.filter(value => readUtf8FromConstantPool(constantPool, value.nameIndex) === methodName)[0]
         const codeAttributes =
             method.attributes.filter(attribute => readUtf8FromConstantPool(constantPool, attribute.attributeNameIndex) === "Code");
@@ -41,7 +40,7 @@ export default class Thread {
         const argsCount = getArgumentsAndReturnType(readUtf8FromConstantPool(constantPool, method.descriptorIndex))[0].length;
 
         const frame = new Frame(this, method, classFile, codeAttribute.maxLocals - argsCount, constantPool, args);
-        this.pc++;
+        this.runtimeDataArea.incrementPCRegister(this.id);
         this.stack.push(frame);
 
         if (this.stack.length > this.stackSize) {
--- src/jvm/models/ClassFile.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=ClassFile.js.map
